c886a25c44000317dabccd3af69a862bcc1928d0
Bingjie Yan <bj.yan.pa@qq.com>
2021-11-10 00:57:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ class CoroutineSpeedup:
 
         self.cache_space = []
 
-        self.max_results = 10
+        self.max_results = 30
 
     def _adaptor(self):
         while not self.worker.empty():
