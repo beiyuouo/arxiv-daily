a1b7ead8bb089d450890a9450f6140a686888bf0
beiyuouo <bj.yan.pa@qq.com>
2022-08-13 10:47:27 +0800

    feat: add history

--- main.py
+++ main.py
@@ -29,9 +29,10 @@ from config import (
     SERVER_DIR_STORAGE,
     SERVER_PATH_README,
     SERVER_PATH_DOCS,
+    SERVER_DIR_HISTORY,
     SERVER_PATH_STORAGE_MD,
     TIME_ZONE_CN,
-    logger
+    logger,
 )
 
 
@@ -54,7 +55,7 @@ class ToolBox:
     def handle_html(url: str):
         headers = {
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.44"
+            "Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.44"
         }
         proxies = {"http": None, "https": None}
         session = requests.session()
@@ -70,9 +71,9 @@ class CoroutineSpeedup:
     """轻量化的协程控件"""
 
     def __init__(
-            self,
-            work_q: Queue = None,
-            task_docker=None,
+        self,
+        work_q: Queue = None,
+        task_docker=None,
     ):
         # 任务容器：queue
         self.worker = work_q if work_q else Queue()
@@ -107,7 +108,7 @@ class CoroutineSpeedup:
         res = arxiv.Search(
             query=keyword_,
             max_results=self.max_results,
-            sort_by=arxiv.SortCriterion.SubmittedDate
+            sort_by=arxiv.SortCriterion.SubmittedDate,
         ).results()
 
         context.update({"response": res, "hook": context})
@@ -127,7 +128,7 @@ class CoroutineSpeedup:
 
             publish_time = result.published.date()
 
-            ver_pos = paper_id.find('v')
+            ver_pos = paper_id.find("v")
             paper_key = paper_id if ver_pos == -1 else paper_id[0:ver_pos]
 
             # 尝试获取仓库代码
@@ -163,25 +164,30 @@ class CoroutineSpeedup:
             #   |publish_time|paper_title|paper_first_author|[paper_id](paper_url)|`[link](url)`
             # ELSE
             #   |publish_time|paper_title|paper_first_author|[paper_id](paper_url)|`null`
-            _paper.update({
-                paper_key: {
-                    "publish_time": publish_time,
-                    "title": paper_title,
-                    "authors": f"{paper_first_author} et.al.",
-                    "id": paper_id,
-                    "paper_url": paper_url,
-                    "repo": repo_url
-                },
-            })
-        self.channel.put_nowait({
-            "paper": _paper,
-            "topic": context["hook"]["topic"],
-            "subtopic": context["hook"]["subtopic"],
-            "fields": ["Publish Date", "Title", "Authors", "PDF", "Code"]
-        })
+            _paper.update(
+                {
+                    paper_key: {
+                        "publish_time": publish_time,
+                        "title": paper_title,
+                        "authors": f"{paper_first_author} et.al.",
+                        "id": paper_id,
+                        "paper_url": paper_url,
+                        "repo": repo_url,
+                    },
+                }
+            )
+        self.channel.put_nowait(
+            {
+                "paper": _paper,
+                "topic": context["hook"]["topic"],
+                "subtopic": context["hook"]["subtopic"],
+                "fields": ["Publish Date", "Title", "Authors", "PDF", "Code"],
+            }
+        )
         logger.success(
             f"handle [{self.channel.qsize()}/{self.max_queue_size}]"
-            f" | topic=`{context['topic']}` subtopic=`{context['hook']['subtopic']}`")
+            f" | topic=`{context['topic']}` subtopic=`{context['hook']['subtopic']}`"
+        )
 
     def offload_tasks(self):
         if self.task_docker:
@@ -203,14 +209,21 @@ class CoroutineSpeedup:
             file_obj[md_obj["hook"]] += md_obj["content"]
 
             # 生成 mkdocs 所需文件
-            os.makedirs(os.path.join(SERVER_PATH_DOCS, f'{context["topic"]}'), exist_ok=True)
-            with open(os.path.join(SERVER_PATH_DOCS, f'{context["topic"]}', f'{context["subtopic"]}.md'), 'w') as f:
+            os.makedirs(
+                os.path.join(SERVER_PATH_DOCS, f'{context["topic"]}'), exist_ok=True
+            )
+            with open(
+                os.path.join(
+                    SERVER_PATH_DOCS, f'{context["topic"]}', f'{context["subtopic"]}.md'
+                ),
+                "w",
+            ) as f:
                 f.write(md_obj["content"])
-               
 
         # 生成 Markdown 模板文件
         template_ = ot.generate_markdown_template(
-            content="".join(list(file_obj.values())))
+            content="".join(list(file_obj.values()))
+        )
         # 存储 Markdown 模板文件
         ot.storage(template_, obj_="database")
 
@@ -238,7 +251,8 @@ class _OverloadTasks:
         self.update_time = ToolBox.log_date(mode="log")
 
         self.storage_path_by_date = SERVER_PATH_STORAGE_MD.format(
-            ToolBox.log_date('file'))
+            ToolBox.log_date("file")
+        )
         self.storage_path_readme = SERVER_PATH_README
         self.storage_path_docs = SERVER_PATH_DOCS
 
@@ -255,18 +269,22 @@ class _OverloadTasks:
         return f"[{text}]({link})"
 
     def _generate_markdown_table_content(self, paper: dict):
-        paper['publish_time'] = f"**{paper['publish_time']}**"
-        paper['title'] = f"**{paper['title']}**"
-        _pdf = self._set_markdown_hyperlink(
-            text=paper['id'], link=paper['paper_url'])
-        _repo = self._set_markdown_hyperlink(
-            text="link", link=paper['repo']) if "http" in paper['repo'] else "null"
+        paper["publish_time"] = f"**{paper['publish_time']}**"
+        paper["title"] = f"**{paper['title']}**"
+        _pdf = self._set_markdown_hyperlink(text=paper["id"], link=paper["paper_url"])
+        _repo = (
+            self._set_markdown_hyperlink(text="link", link=paper["repo"])
+            if "http" in paper["repo"]
+            else "null"
+        )
 
-        line = f"|{paper['publish_time']}" \
-               f"|{paper['title']}" \
-               f"|{paper['authors']}" \
-               f"|{_pdf}" \
-               f"|{_repo}|\n"
+        line = (
+            f"|{paper['publish_time']}"
+            f"|{paper['title']}"
+            f"|{paper['authors']}"
+            f"|{_pdf}"
+            f"|{_repo}|\n"
+        )
 
         return line
 
@@ -285,12 +303,12 @@ class _OverloadTasks:
         @return:
         """
         path_factory = {
-            'database': self.storage_path_by_date,
-            'readme': self.storage_path_readme,
-            'docs': self.storage_path_docs
+            "database": self.storage_path_by_date,
+            "readme": self.storage_path_readme,
+            "docs": self.storage_path_docs,
         }
         if obj_ not in path_factory.keys():
-            path_ = path_factory['readme']
+            path_ = path_factory["readme"]
         else:
             path_ = path_factory[obj_]
         with open(path_, "w", encoding="utf8") as f:
@@ -300,10 +318,14 @@ class _OverloadTasks:
     def generate_markdown_template(self, content: str):
         _project = f"# arxiv-daily\n"
         _pin = f" Automated deployment @ {self.update_time} Asia/Shanghai\n"
-        _tos = "> Welcome to contribute! Add your topics and keywords in " \
-               "[`topic.yml`](https://github.com/beiyuouo/arxiv-daily/blob/main/database/topic.yml).\n"
-        _tos += "> You can also view historical data through the " \
-                "[storage](https://github.com/beiyuouo/arxiv-daily/blob/main/database/storage).\n"
+        _tos = (
+            "> Welcome to contribute! Add your topics and keywords in "
+            "[`topic.yml`](https://github.com/beiyuouo/arxiv-daily/blob/main/database/topic.yml).\n"
+        )
+        _tos += (
+            "> You can also view historical data through the "
+            "[storage](https://github.com/beiyuouo/arxiv-daily/blob/main/database/storage).\n"
+        )
 
         _form = _project + _pin + _tos + content
 
@@ -319,8 +341,12 @@ class _OverloadTasks:
         _subtopic_md = f"\n### {_subtopic}\n"
         _fields_md = f"|{'|'.join(_fields)}|\n"
         _style_md = f"|{'|'.join([self._set_style_to('center') for _ in range(len(_fields))])}|\n"
-        table_lines = "".join([self._generate_markdown_table_content(
-            paper) for paper in _paper_obj.values()])
+        table_lines = "".join(
+            [
+                self._generate_markdown_table_content(paper)
+                for paper in _paper_obj.values()
+            ]
+        )
 
         _content_md = _subtopic_md + _fields_md + _style_md + table_lines
 
@@ -348,8 +374,11 @@ class Scaffold:
         context = ToolBox.get_yaml_data()
 
         # Set tasks
-        pending_atomic = [{"subtopic": subtopic, "keyword": keyword.replace('"', ""), "topic": topic}
-                          for topic, subtopics in context.items() for subtopic, keyword in subtopics.items()]
+        pending_atomic = [
+            {"subtopic": subtopic, "keyword": keyword.replace('"', ""), "topic": topic}
+            for topic, subtopics in context.items()
+            for subtopic, keyword in subtopics.items()
+        ]
 
         # Offload tasks
         booster = CoroutineSpeedup(task_docker=pending_atomic)
@@ -363,8 +392,20 @@ class Scaffold:
             with open(SERVER_PATH_README, "w", encoding="utf8") as f:
                 for i in template_:
                     f.write(i)
-            
-            shutil.copyfile(SERVER_PATH_README, os.path.join(SERVER_PATH_DOCS, "index.md"))
+
+            shutil.copyfile(
+                SERVER_PATH_README, os.path.join(SERVER_PATH_DOCS, "index.md")
+            )
+            # make history directory and copy files in SERVER_DIR_STORAGE to history directory
+            if not os.path.exists(SERVER_DIR_HISTORY):
+                os.mkdir(SERVER_DIR_HISTORY)
+            for file in os.listdir(SERVER_DIR_STORAGE):
+                file_format = os.path.basename(file).split("_")[-1]
+                shutil.copyfile(
+                    os.path.join(SERVER_DIR_STORAGE, file),
+                    os.path.join(SERVER_DIR_HISTORY, file_format),
+                )
+
 
 if __name__ == "__main__":
     Fire(Scaffold)
